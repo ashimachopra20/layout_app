e2f216d5d29bcbbf937791e504c49cd08263cffe
ashimachopra20 <64543480+ashimachopra20@users.noreply.github.com>
2020-07-28 18:04:26 +0530

    Update and rename README.md to main.dart

---
+++ main.dart
@@ -0,0 +1,12 @@
+# layout_app
+import 'package:flutter/material.dart';
+import 'ui/home.dart';
+void main() {
+  runApp(x());
+}
+class x extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MyApp();
+  }
+}
